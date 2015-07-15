6ed75cc87a48b89454b16ef0f52df6b450be7275
José Stiller <info@josestiller.de>
2015-07-15 23:40:55 +0200

    removed comments

--- base.js
+++ base.js
@@ -1,54 +1,36 @@
 var base = base || {};
 
-/**
- * Already Seen
- * 
- * This function should track all websites and mark them as seen
- * 
- * @author José Stiller
- * @email info@josestiller.de
- * @retun void
- */
 base.alreadySeen = function(config) {
+    'use strict'
+    
     var self = this;
     self.config = config || {
-            rules: [
-                {
-                    location: '',
-                    exceptions: []
-                }
-            ],
-            prefix: 'visited-',
-            data: 'visited'
-        };
+        rules: [
+            {
+                location: '',
+                exceptions: []
+            }
+        ],
+        prefix: 'visited-',
+        data: 'visited'
+    };
     
-    /**
-     * Track
-     * 
-     * Should track all visited sites that are configurated
-     * excluding the exceptions
-     */
     self.track = function (client) {
-        // iterate trough all defined rules
         self.config.rules.forEach(function(track) {
-            var ytrack = true;
+            var permission = true;
 
-            // checks if the rule matches
             if (client.substr(0, track.location.length) === track.location) {
-                // iterate trough all exceptions
                 if(typeof track.exceptions === "object") {
                     track.exceptions = [track.exceptions];
                 }
                 
                 track.exceptions.forEach(function(exception) {
-                    // check if the exception matches
                     if (client.substr(0, exception.location.length) === exception.location) {
-                        ytrack = false;
+                        permission = false;
                     }
                 });
                 
-                // store path if none of the exceptions matched
-                if (ytrack === true) {
+                if (permission === true) {
                     localStorage.setItem(self.config.prefix + window.location.pathname, true);
                 }
             }
@@ -59,13 +41,6 @@ base.alreadySeen = function(config) {
         if(!Array.isArray(links)) {
             links = Object.keys(links).map(function (key) {return links[key]});
         }
-        
-        // iterate trough all links and check if it was already visited
-        // for (var i = 0; i < links.length; i++) {
-        //     if (links[i].host == window.location.host && localStorage.getItem(self.config.prefix + links[i].pathname)) {
-        //         links[i].dataset.visited = true;
-        //     }
-        // };
         
         links.forEach(function(link) {
             if (link.host == window.location.host && localStorage.getItem(self.config.prefix + link.pathname)) {
